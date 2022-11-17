fe2a6d6585e25b76e96343b5530b9a808e6314d0
sayyyyyy <d958956a6b650@gmail.com>
2022-11-17 22:41:27 +0900

    pages, componentsディレクトリ作成、開発の準備完了

--- src/app.vue
+++ src/app.vue
@@ -2,5 +2,6 @@
   <div>
     <!-- <NuxtWelcome /> -->
     <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1>
+    <NuxtPage />
   </div>
 </template>
---
+++ src/pages/top.vue
@@ -0,0 +1,3 @@
+<template>
+    <ProgrammingLangBtn></ProgrammingLangBtn>
+</template>
