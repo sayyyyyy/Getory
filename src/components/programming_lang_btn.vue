94edffdfdb3cbea9bd44cd6a012eb18f90140fc8
sayyyyyy <d958956a6b650@gmail.com>
2022-11-19 22:08:06 +0900

    親コンポーネントからデータの取得

--- src/components/programming_lang_btn.vue
+++ src/components/programming_lang_btn.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="programming-lang-btns">
-        <!-- <img src=""></img> -->
-        <p></p>
+        <img :src="lang_data.img">
+        <p>{{lang_data.lang}}</p>
     </div>
 </template>
 
 <script setup lang="ts">
-const programming_languages = ['']
+const props_data = defineProps(['lang_data'])
+
+
+
+// console.log(lang_data)
+
+
 </script>
--- src/pages/top.vue
+++ src/pages/top.vue
@@ -1,14 +1,21 @@
 <template>
-    <ProgrammingLangBtn></ProgrammingLangBtn>
-    <button>叩く</button>
+    <ProgrammingLangBtn ref="childRef" v-for="programming_language in programming_languages" :lang_data="programming_language"></ProgrammingLangBtn>
+    <!-- <ul>
+        <li v-for="programming_language in programming_languages">{{programming_language.lang}}</li>
+    </ul> -->
 </template>
 
 <script setup lang="ts">
     import { getRepositoryByLang, getRepositoryByUpdate } from '../scripts/apiController'
 
+
     // const random_repos =  getRepositoryByUpdate()
     // console.log(random_repos.value)
-
-    const lang_repo = getRepositoryByLang('js')
-    console.log(lang_repo.value.items)
+    const programming_languages = [
+        {lang: 'HTML', img: '../assets/img/HTML.png'},
+        {lang: 'TypeScript', img: '../assets/img/HTML.png'}
+    ]
+    const childRef = ref()
+    // const lang_repo = getRepositoryByLang('js')
+    // console.log(lang_repo.value.items)
 </script>
