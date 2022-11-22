1a31a83117fff5efc5c1e26ebe29243a0d83284e
sayyyyyy <d958956a6b650@gmail.com>
2022-11-22 16:23:25 +0900

    faviconの追加、反映

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            link: [
+                { rel: "icon", type: "image/png", href: "/favicon.png" },
+              ],
+        }
+    }
 })
