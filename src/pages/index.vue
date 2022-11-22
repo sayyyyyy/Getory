6777b6ce9b4defc29f4531d48678f50ae05ffcbf
sayyyyyy <d958956a6b650@gmail.com>
2022-11-23 08:36:34 +0900

    タイトル表示を正常に

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="w-screen">
-        <div class="flex bg-neutral-800 border border-green-500 rounded-r-md text-m w-4/5 mb-4">
-            <p class="text-white">{{ $t('searched_by_lang') }}</p>
-            <input type="text" autocomplete="off">
+        <div class="flex bg-neutral-800 border border-green-500 rounded-r-xl text-lg w-5/6 mb-4 items-center h-10">
+            <p class="text-white ml-2">{{ $t('searched_by_lang') }}</p>
+            <input type="text" autocomplete="off" class="bg-inherit text-white border-b-2 border-gray-500/100 ml-4 h-5 w-32">
+            <img src="/search_icon.png" class="w-5 h-5 ml-2">
         </div>
         <div class="flex overflow-x-scroll text-xs relative mb-4 w-screen">
             <ProgrammingLangBtn ref="childRef" v-for="programming_language in programming_languages" :lang_data="programming_language"></ProgrammingLangBtn>
         </div>
-        <div class="flex bg-neutral-800 border border-green-500 rounded-r-md text-m w-1/2 text-center mb-4">
-            <p class="text-white">{{ $t('latest_repo') }}</p>
+        <div class="flex bg-neutral-800 border border-green-500 rounded-r-xl text-lg w-5/6 mb-4 items-center h-10">
+            <p class="text-white ml-2">{{ $t('latest_repo') }}</p>
         </div>
         <div class="flex flex-col">
             <Repository_component ref="childRef" v-for="repository_data in state.repo_data" :repository_data="repository_data"></Repository_component>
