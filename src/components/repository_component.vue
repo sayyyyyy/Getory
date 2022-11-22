8bf30d0513c26c91612112b46d36ab4fa6adf8e6
sayyyyyy <d958956a6b650@gmail.com>
2022-11-23 00:05:37 +0900

    ユーザーアイコンの表示

--- src/components/repository_component.vue
+++ src/components/repository_component.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="repository-components bg-neutral-800 rounded-2xl drop-shadow-md  text-white m-1" @click="transitionGitHub(repository_data.html_url)">
-        <div class="repository-container-left">
-            <p>{{ repository_data.name}}</p>
-            <p>{{ repository_data.owner.login}}</p>
+        <div class="repository-container-top flex">
+            <div class="repository-container-left">
+                <p>{{ repository_data.name}}</p>
+            </div>
+            <!-- <div class="repository-container-right">
+                <span>Star</span><span>{{ repository_data.stargazers_count}}</span>
+                <span>Folk</span><span>{{ repository_data.forks_count}}</span>
+            </div> -->
         </div>
-        <!-- <div class="repository-container-right">
+        <div class="repository-container-bottom flex">
+            <div class="flex">
+                <p>{{ repository_data.owner.login}}</p>
+                <img class="w-10 h=10" :src="repository_data.owner.avatar_url">
+            </div>
+            <!-- <div class="flex">
+                <p>{{repository_data.language}}</p>
+            </div> -->
+            <!-- <p>Updated in {{repository_data.updated_at}}</p> -->
+        </div>
 
-        </div> -->
     </div>
 </template>
 
@@ -16,4 +29,9 @@ const repo = defineProps(['repository_data'])
 const transitionGitHub = (url: string) => {
     window.location.href = url
 }
+
+// const generateImgPath = (fileName: string): string => {
+//     const new_url = new URL(`{fileName}`)
+//     return new_url
+// }
 </script>
