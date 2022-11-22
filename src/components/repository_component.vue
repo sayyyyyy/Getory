3c0c0cb263fbd27ab3f78370cf4f3c4429d35b12
sayyyyyy <d958956a6b650@gmail.com>
2022-11-22 11:00:49 +0900

    表示するタイトルをリポジトリ名のみに変更

--- src/components/repository_component.vue
+++ src/components/repository_component.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="repository-components border-2 border-black w-40 h-40" @click="transitionGitHub(repository_data.html_url)">
         <div class="repository-container-left">
-            <p>{{ repository_data.full_name}}</p>
+            <p>{{ repository_data.name}}</p>
             <p>{{ repository_data.owner.login}}</p>
         </div>
         <div class="repository-container-right">
