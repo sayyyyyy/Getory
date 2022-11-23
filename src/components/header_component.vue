a0435b97c3162f2336f6e5e3a38071e693641ff6
sayyyyyy <d958956a6b650@gmail.com>
2022-11-23 09:08:17 +0900

    header調整

--- src/components/header_component.vue
+++ src/components/header_component.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="flex mb-8">
-        <img class="w-7 h-7" src="/favicon.png">
-        <button class="bg-neutral-800 text-white text-xs">{{ $t('random_btn')}}</button>
-        <!-- TODO デザインモード切り替えを追加 -->
-        <form>
-            <select id="locale-select" class="text-xs" v-model="$i18n.locale">
-                <option value="en">{{ $t('en') }}</option>
-                <option value="ja">{{ $t('ja') }}</option>
-            </select>
-        </form>
+    <div class="flex p-4 mb-8 items-center">
+        <img class="w-10 h-10" src="/favicon.png">
+        <div class="flex ml-auto text-sm text-white items-center">
+            <button class="bg-neutral-800 rounded-2xl drop-shadow-xl mr-2 p-2 h-4/5">{{ $t('random_btn')}}</button>
+            <!-- TODO デザインモード切り替えを追加 -->
+            <form>
+                <select id="locale-select" class="h-4/5 bg-inherit text-base" v-model="$i18n.locale">
+                    <option value="en">{{ $t('en') }}</option>
+                    <option value="ja">{{ $t('ja') }}</option>
+                </select>
+            </form>
+        </div>
     </div>
 
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="w-screen">
-        <div class="flex bg-neutral-800 border border-green-500 border-l-0 rounded-r-xl text-lg w-5/6 mb-6 items-center h-10">
+        <div class="flex bg-neutral-800 border border-green-500 border-l-0 rounded-r-xl text-lg w-80 min-w-fit mb-6 items-center h-10">
             <p class="text-white ml-4">{{ $t('searched_by_lang') }}</p>
-            <input type="text" autocomplete="off" class="bg-inherit text-white border-b-2 border-gray-500/100 ml-4 h-5 w-32">
-            <img src="/search_icon.png" class="w-5 h-5 ml-2">
+            <input type="text" autocomplete="off" class="bg-inherit text-white border-b-2 border-gray-500/100 ml-4 h-5 w-28">
+            <img src="/search_icon.png" class="w-5 h-5 ml-2 mr-2">
         </div>
         <div class="flex overflow-x-scroll text-xs relative mb-10 w-screen">
             <ProgrammingLangBtn ref="childRef" v-for="programming_language in programming_languages" :lang_data="programming_language"></ProgrammingLangBtn>
         </div>
-        <div class="flex bg-neutral-800 border border-green-500 border-l-0 rounded-r-xl text-lg w-1/2 mb-6 items-center h-10">
+        <div class="flex bg-neutral-800 border border-green-500 border-l-0 rounded-r-xl text-lg w-48 mb-6 items-center h-10">
             <p class="text-white ml-4">{{ $t('latest_repo') }}</p>
         </div>
         <div class="flex flex-col">
