cbab5e8c85d1d9f84de5f0121397acaf98a20e26
sayyyyyy <d958956a6b650@gmail.com>
2022-11-23 20:21:39 +0900

    iconの追加

--- src/components/repository_component.vue
+++ src/components/repository_component.vue
@@ -1,23 +1,34 @@
 <template>
     <div class="repository-components cursor-pointer bg-neutral-800 rounded-md drop-shadow-md text-white m-2" @click="transitionGitHub(repository_data.node.url)">
         <div class="repository-container-top flex ml-2">
-            <div class="repository-container-left">
-                <p class="text-lg">{{ repository_data.node.name }}</p>
+            <div class="repository-container-left w-3/4">
+                <p class="text-xl">{{ repository_data.node.name }}</p>
+                <p class="text-sm">{{changeDescription(repository_data.node.description)}}</p>
             </div>
-            <div class="repository-container-right">
-                <span>Star</span><span>{{ repository_data.node.stargazerCount}}</span>
-                <span>Folk</span><span>{{ repository_data.node.forkCount}}</span>
+            <div class="repository-container-right flex flex-col">
+                <div class="flex">
+                    <img src="star.png">
+                    <span>{{ repository_data.node.stargazerCount}}</span>
+                </div>
+                <div class="flex">
+                    <img src="fork.png">
+                    <span>{{ repository_data.node.forkCount}}</span>
+                </div>
+                
             </div>
         </div>
         <div class="repository-container-bottom flex ml-2">
             <div class="flex items-center">
                 <p>{{ repository_data.node.owner.login}}</p>
-                <img class="w-10 h-10 rounded-3xl" :src="repository_data.node.owner.avatarUrl">
+                <img class="w-8 h-8 rounded-3xl" :src="repository_data.node.owner.avatarUrl">
             </div>
             <div class="flex">
                 <p>{{ checkLang(repository_data.node.languages.nodes[0])}}</p>
             </div>
-            <p>Updated in {{repository_data.node.updatedAt.slice(0, 10)}}</p>
+            <div class="flex">
+                <img src="calender.png">
+                <p>{{repository_data.node.updatedAt.slice(0, 10)}}</p>
+            </div>
         </div>
 
     </div>
@@ -30,6 +41,15 @@ const transitionGitHub = (url: string) => {
     window.location.href = url
 }
 
+const changeDescription = (description: string) => {
+    try {
+        if (description)
+            return description.slice(0, 50) + '...'
+        return ''
+    } catch {
+        return ''
+    }
+}
 const checkLang = (lang: Object) => {
     try {
         if (lang.name)
