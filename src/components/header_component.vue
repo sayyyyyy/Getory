2048e1a02f5cfa44e97678b7def88b168597e747
sayyyyyy <d958956a6b650@gmail.com>
2022-11-23 09:12:01 +0900

    search画面へタイトル、ヘッダーの適用、英語にしてもずれないように

--- src/components/header_component.vue
+++ src/components/header_component.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex p-4 mb-8 items-center">
+    <div class="flex p-4 mb-8 items-center w-100">
         <img class="w-10 h-10" src="/favicon.png">
         <div class="flex ml-auto text-sm text-white items-center">
             <button class="bg-neutral-800 rounded-2xl drop-shadow-xl mr-2 p-2 h-4/5">{{ $t('random_btn')}}</button>
--- src/pages/search.vue
+++ src/pages/search.vue
@@ -1,5 +1,7 @@
 <template>
-    <p class="text-white">{{ $t(state.search_way, { lang: state.repo_data[0].language }) }}</p>
+    <div class="flex bg-neutral-800 border border-green-500 border-l-0 rounded-r-xl text-lg w-64 min-w-fit mb-6 items-center h-10">
+        <p class="text-white ml-4">{{ $t(state.search_way, { lang: state.repo_data[0].language }) }}</p>
+    </div>
     <div class="flex flex-col">
         <Repository_component v-for="repository_data in state.repo_data" :repository_data="repository_data"></Repository_component>
     </div>
