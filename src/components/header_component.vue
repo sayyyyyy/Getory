6537414a45255be3084c1e2f14f172aca622f7c8
sayyyyyy <d958956a6b650@gmail.com>
2022-11-25 13:59:25 +0900

    切り替えボタンの追加

--- src/components/header_component.vue
+++ src/components/header_component.vue
@@ -4,7 +4,10 @@
         <div class="flex ml-auto items-center">
             <button @click="getRandomRepo" class="bg-neutral-800 rounded-2xl drop-shadow-xl mr-2 p-2 h-4/5 text-sm text-white lg:text-base lg:mr-6">{{ $t('random_btn')}}</button>
             <!-- TODO デザインモード切り替えを追加 -->
-            <button @click="setColorTheme($colorMode.preference == 'dark' ?'light' : 'dark')" class="text-black dark:text-white">モード</button>
+            <label class="inline-flex relative items-center cursor-pointer">
+                <input @click="setColorTheme($colorMode.preference == 'dark' ?'light' : 'dark')" type="checkbox" value="" class="sr-only peer">
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
             <form>
                 <select id="locale-select" class="cursor-pointer h-4/5 bg-inherit text-base text-black lg:text-lg lg:mr-4 dark:text-white" v-model="$i18n.locale">
                     <option value="en">{{ $t('en') }}</option>
